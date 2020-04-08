37420b88dbffca2e6c4dc202ce9ba7da3211b76e
Hosein Hashemi <hosein.hashemi@protonmail.ch>
2020-04-08 16:32:48 +0200

    Update Adabound.py

--- Adabound.py
+++ Adabound.py
@@ -3,23 +3,9 @@ os.environ['TF_KERAS']='1'
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.python.keras import utils
-from tensorflow.python.keras import activations
-from tensorflow.python.keras import applications
 from tensorflow.keras import backend as K
-from tensorflow.python.keras import datasets
 from tensorflow.python.keras import engine
-from tensorflow.python.keras import layers
-from tensorflow.python.keras import preprocessing
-from tensorflow.python.keras import wrappers
-from tensorflow.python.keras import callbacks
-from tensorflow.python.keras import constraints
-from tensorflow.python.keras import initializers
-from tensorflow.python.keras import metrics
-from tensorflow.python.keras import models
-from tensorflow.python.keras import losses
 from tensorflow.keras import optimizers
-from tensorflow.python.keras import regularizers
 
 class adabound(optimizers.Optimizer):
     #AdaBound optimizer.
